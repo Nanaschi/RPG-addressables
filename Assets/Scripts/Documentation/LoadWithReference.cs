23729be800cc9e5563b13da6811365cd07c24db1
Efremov Danil <naturforscher@outlook.com>
2022-01-21 22:26:55 +0300

    Implemented the documentation scripts (address/ AssetReference/ Label load)

---
+++ Assets/Scripts/Documentation/LoadWithReference.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class LoadWithReference : MonoBehaviour
+{
+    // Assign in Editor
+    public AssetReference reference;
+
+    // Start the load operation on start
+    void Start() {
+        AsyncOperationHandle handle = reference.LoadAssetAsync<GameObject>();
+        handle.Completed += Handle_Completed;
+    }
+
+    // Instantiate the loaded prefab on complete
+    private void Handle_Completed(AsyncOperationHandle obj) {
+        if (obj.Status == AsyncOperationStatus.Succeeded) {
+            Instantiate(reference.Asset, transform);
+        } else {
+            Debug.LogError($"AssetReference {reference.RuntimeKey} failed to load.");
+        }
+    }
+
+    // Release asset when parent object is destroyed
+    private void OnDestroy() {
+        reference.ReleaseAsset();
+    }
+}
