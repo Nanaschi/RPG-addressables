0a7ecdf0cbb1c82fc2d99d1d84da71ab8efb97dd
Efremov Danil <naturforscher@outlook.com>
2022-01-31 12:16:06 +0300

    0.1

--- Assets/Scripts/Real Game Prototypes/2Iterating through various references/Scripts/ModelItem.cs
+++ Assets/Scripts/Real Game Prototypes/2Iterating through various references/Scripts/ModelItem.cs
@@ -1,19 +1,33 @@
 using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Serialization;
 using Object = UnityEngine.Object;
 
 
 [Serializable]
 public class ModelItem
 {
-    public GameObject Model;
-    public AssetReference ModelReference;
-    public Object ModelReferenceProperty
+
+
+    [SerializeField] [FormerlySerializedAs("Model")] private GameObject _model;
+
+    public GameObject Model
     {
-        get { return ModelReference.Asset;}
-        set {  }
+        get => _model;
+        set => _model = value;
     }
+
+    [SerializeField] [FormerlySerializedAs("ModelReference")] private AssetReference _modelReference;
+    public AssetReference ModelReference
+    {
+        get => _modelReference;
+        set => _modelReference = value;
+    }
+    
+    
+    
+    
     public int Rotation;
     public float Scale = 1;
     public Vector3 Position;
--- Assets/Scripts/Real Game Prototypes/4 Reference Logic/GameObjectInstantiation.cs
+++ Assets/Scripts/Real Game Prototypes/4 Reference Logic/GameObjectInstantiation.cs
@@ -2,17 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class GameObjectInstantiation : MonoBehaviour
 {
     [SerializeField] private AssetRefObjectData _assetRefObjectData;
     [SerializeField] private int _objectIndex;
-
+    [SerializeField] private Transform[] _pointsToInstantiate;
     private void Start()
     {
         for (int i = 0; i < _assetRefObjectData._setOfTileItems.Items.Count; i++)
         {
-            Instantiate(_assetRefObjectData._setOfTileItems.Items[i].ModelItem.Model);
+            Instantiate(_assetRefObjectData._setOfTileItems.Items[i].ModelItem.Model, _pointsToInstantiate[UnityEngine.Random.Range(0, _pointsToInstantiate.Length)]);
         }
 
     }
