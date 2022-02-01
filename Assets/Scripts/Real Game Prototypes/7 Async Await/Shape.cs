f218e38da3625f8a0f8a9688f13e70b256d2a9a2
Efremov Danil <naturforscher@outlook.com>
2022-02-01 15:46:19 +0300

    Making the code wait until all tasks are performed!

--- Assets/Scripts/Real Game Prototypes/7 Async Await/Shape.cs
+++ Assets/Scripts/Real Game Prototypes/7 Async Await/Shape.cs
@@ -8,11 +8,28 @@ public class Shape : MonoBehaviour
 
     [SerializeField] private int _secondsToRotate;
     [SerializeField] private GameObject[] _objectsToRotate;
+    [SerializeField] private GameObject _finishedText;
     public async void BeginRotate()
+    {
+        _finishedText.SetActive(false);
+        var tasks = new Task[_objectsToRotate.Length];
+        for (int i = 0; i < _objectsToRotate.Length; i++)
+        {
+            tasks[i] = RotateForSeconds(_secondsToRotate + i, _objectsToRotate[i].transform);
+        }
+
+        await Task.WhenAll(tasks);
+        
+        _finishedText.SetActive(true);
+
+
+    }  
+    
+    public void BeginRotateAllTogether()
     {
         for (int i = 0; i < _objectsToRotate.Length; i++)
         {
-           await RotateForSeconds(_secondsToRotate, _objectsToRotate[i].transform);
+           RotateForSeconds(_secondsToRotate, _objectsToRotate[i].transform);
         }
             
     }
@@ -26,8 +43,8 @@ public class Shape : MonoBehaviour
             transform.Rotate( _rotationAngle* Time.deltaTime *150);
             yield return null;
         }
-    }   */ 
-    
+    }   */
+
     private async Task RotateForSeconds(float duration, Transform objectToRotate) //Task gives us the information about the performed actions
     {
         var _rotationAngle = new Vector3(1, 1);
@@ -35,7 +52,7 @@ public class Shape : MonoBehaviour
         while (Time.time < end)
         {
             objectToRotate.Rotate( _rotationAngle * Time.deltaTime *150);
-            await Task.Yield();
+             await Task.Yield();
         }
     }
 }
