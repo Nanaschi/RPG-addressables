2568ed5c9674161af74736840a136570e42ebe17
Efremov Danil <naturforscher@outlook.com>
2022-02-01 14:55:03 +0300

    Async await practice (A chain of Actions!)

--- Assets/Scripts/Real Game Prototypes/7 Async Await/Shape.cs
+++ Assets/Scripts/Real Game Prototypes/7 Async Await/Shape.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
 
     [SerializeField] private int _secondsToRotate;
-    public void BeginRotate()
+    [SerializeField] private GameObject[] _objectsToRotate;
+    public async void BeginRotate()
     {
-        StartCoroutine(RotateForSeconds(_secondsToRotate));
+        for (int i = 0; i < _objectsToRotate.Length; i++)
+        {
+           await RotateForSeconds(_secondsToRotate, _objectsToRotate[i].transform);
+        }
+            
     }
 
-    private IEnumerator RotateForSeconds(float duration)
+    /*private IEnumerator RotateForSeconds(float duration)
     {
+        var _rotationAngle = new Vector3(1, 1);
         var end = Time.time + duration;
         while (Time.time < end)
         {
-            transform.Rotate(new Vector3(1,1) * Time.deltaTime *150);
+            transform.Rotate( _rotationAngle* Time.deltaTime *150);
             yield return null;
+        }
+    }   */ 
+    
+    private async Task RotateForSeconds(float duration, Transform objectToRotate) //Task gives us the information about the performed actions
+    {
+        var _rotationAngle = new Vector3(1, 1);
+        var end = Time.time + duration;
+        while (Time.time < end)
+        {
+            objectToRotate.Rotate( _rotationAngle * Time.deltaTime *150);
+            await Task.Yield();
         }
     }
 }
